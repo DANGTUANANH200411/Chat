a3782a451904bc9539ed1377a0c556cf77e0dba0
DESKTOP-1T6EOMT\Admin <anh.dangtuan@poscodx.net>
2024-10-03 10:32:37 +0700

    update url message

--- src/components/Chat/body/content-render/UrlMessage.tsx
+++ src/components/Chat/body/content-render/UrlMessage.tsx
@@ -1,7 +1,7 @@
 import { Row, Typography } from 'antd';
 import { getLinkPreview } from 'link-preview-js';
 import React, { useEffect, useMemo, useState } from 'react';
-import './style.css'
+import './style.css';
 interface Props {
 	id: string;
 	content: string;
@@ -16,23 +16,32 @@ function UrlMessage(props: Props) {
 
 	useEffect(() => {
 		if (!viewData) return;
-		document.getElementById(id)!.classList.add('short')
-	}, [viewData])
-	
+		document.getElementById(id)!.classList.add('short');
+	}, [viewData]);
+
 	return !viewData ? (
 		<a href={content} target='_blank'>
 			{content}
 		</a>
 	) : (
-		<Row className='preview-link-wrapper' onClick={() => window.open(viewData.url, '_blank')}>
-			<img src={viewData.images[0] ?? viewData.favicons[0]} width={'100%'}/>
-			<Row className='flex-grow' style={{padding: 4}}>
-				<Typography.Text strong ellipsis className='preview-title'>{viewData.title}</Typography.Text>
-				<Typography.Paragraph type='secondary' ellipsis={{rows: 4}} >{viewData.description}</Typography.Paragraph>
+		<Row
+			className='preview-link-wrapper'
+			onClick={(e) => {
+				e.preventDefault();
+				window.open(viewData.url, '_blank');
+			}}
+		>
+			<img src={viewData.images[0] ?? viewData.favicons[0]} width={'100%'} />
+			<Row className='flex-grow' style={{ padding: 4, marginBottom: 8 }}>
+				<Typography.Text strong ellipsis className='preview-title'>
+					{viewData.title}
+				</Typography.Text>
+				<Typography.Paragraph type='secondary' ellipsis={{ rows: 4 }} style={{ margin: 0 }}>
+					{viewData.description}
+				</Typography.Paragraph>
 			</Row>
 		</Row>
 	);
-
 }
 
 export default React.memo(UrlMessage);
